a7ff10fdb51accf84ac9ce4e21b7ac933afbd561
thomasgeier03 <thomas.geier@siag.it>
2024-08-20 10:51:22 +0200

    creation

---
+++ API-Test.py
@@ -0,0 +1,100 @@
+import http.client
+import json
+import csv
+import os
+from dotenv import load_dotenv
+import SACAccess
+
+load_dotenv(dotenv_path='api.env')
+
+# Service-Aufruf und Daten in CSV-Datei speichern
+def Usersrequest(bearer_token):
+    conn = http.client.HTTPSConnection(f"{os.getenv('SACServiceURL')}")
+    headers = {
+        #'x-csrf-token': f'{xcsrf_token}',
+        'x-sap-sac-custom-auth': 'true',
+        'Authorization': f'Bearer {bearer_token}'
+    }
+    conn.request("GET", "/api/v1/scim/Users/THOMASGEIER", headers=headers)
+    response = conn.getresponse()
+    data = response.read()
+
+    if response.status == 200:
+        json_data = json.loads(data.decode("utf-8"))  # Decode and parse the response as JSON
+        return json_data
+    else:
+        raise Exception(f"Failed to get data: {response.status} {data.decode('utf-8')}")
+
+    
+def Activitiesrequest(bearer_token):
+    conn = http.client.HTTPSConnection(f"{os.getenv('SACServiceURL')}")
+    headers = {
+        #'x-csrf-token': f'{xcsrf_token}',
+        'x-sap-sac-custom-auth': 'true',
+        'Authorization': f'Bearer {bearer_token}'
+    }
+    conn.request("GET", "/api/v1/audit/activities/exportActivities?pageSize=50", headers=headers)
+    response = conn.getresponse()
+    data = response.read()
+
+    if response.status == 200:
+        return data.decode("utf-8")
+    else:
+        raise Exception(f"Failed to get data: {response.status} {data.decode('utf-8')}")
+    
+def Storiesrequest(bearer_token):
+    conn = http.client.HTTPSConnection(f"{os.getenv('SACServiceURL')}")
+    headers = {
+        #'x-csrf-token': f'{xcsrf_token}',
+        'x-sap-sac-custom-auth': 'true',
+        'Authorization': f'Bearer {bearer_token}'
+    }
+    conn.request("GET", "/api/v1/stories", headers=headers)
+    response = conn.getresponse()
+    data = response.read()
+
+    if response.status == 200:
+        return json.loads(data.decode("utf-8"))  # Decode and parse the response as JSON
+    else:
+        raise Exception(f"Failed to get data: {response.status} {data.decode('utf-8')}")
+    
+
+# CSV-Datei schreiben
+def write_text_to_csv(text_data, csv_filename):
+    """
+    Write plain text data to a CSV file.
+
+    :param text_data: The plain text data to write to the CSV file.
+    :param csv_filename: The name of the CSV file to write to.
+    """
+    if not text_data:
+        raise ValueError("No data to write to CSV")
+
+    lines = text_data.splitlines()
+    with open(csv_filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        
+        for line in lines:
+            writer.writerow([line])
+
+
+# Example usage in the main function
+def main():
+    SACAccess.get_bearer_token()
+    SACAccess.get_xcsrf_token()
+
+    # Activities Request
+    json_data = Activitiesrequest(os.getenv('bearer_token'))
+    write_text_to_csv(json_data, 'activities.csv')
+
+    # Users Request
+    json_data = Usersrequest(os.getenv('bearer_token'))
+    write_text_to_csv(json.dumps(json_data, indent=4), 'users.csv')
+
+    # Stories Request
+    json_data = Storiesrequest(os.getenv('bearer_token'))
+    write_text_to_csv(json.dumps(json_data, indent=4), 'stories.csv')
+
+# Run the main function if the script is executed
+if __name__ == '__main__':
+    main()
---
+++ SACAccess.py
@@ -0,0 +1,80 @@
+import os
+import http.client
+import base64
+import json
+from datetime import datetime, timedelta
+
+TOKEN_FILE = 'bearer_token.json'
+
+def save_token(token, timestamp):
+    with open('api.env', 'r') as file:
+        lines = file.readlines()
+
+    with open('api.env', 'w') as file:
+        for line in lines:
+            if line.startswith('bearer_token='):
+                file.write(f'bearer_token={token}\n')
+            elif line.startswith('bearer_timestamp='):
+                file.write(f'bearer_timestamp={timestamp}\n')
+            else:
+                file.write(line)
+
+        if not any(line.startswith('bearer_token=') for line in lines):
+            file.write(f'bearer_token={token}\n')
+        if not any(line.startswith('bearer_timestamp=') for line in lines):
+            file.write(f'bearer_timestamp={timestamp}\n')
+
+def load_token():
+    token = None
+    timestamp = None
+    if os.path.exists('api.env'):
+        with open('api.env', 'r') as file:
+            for line in file:
+                if line.startswith('bearer_token='):
+                    token = line.strip().split('=')[1]
+                elif line.startswith('bearer_timestamp='):
+                    timestamp = datetime.fromisoformat(line.strip().split('=')[1])
+    return token, timestamp
+
+def get_bearer_token():
+    token, timestamp = load_token()
+    if token and timestamp and datetime.now() - timestamp < timedelta(hours=1):
+        return token
+    else:
+        conn = http.client.HTTPSConnection(f"{os.getenv('SACTokenURL')}")
+        headers = {
+            'Authorization': 'Basic ' + base64.b64encode(f"{os.getenv('User')}:{os.getenv('Secret')}".encode('utf-8')).decode('utf-8')
+        }
+        payload = 'grant_type=client_credentials'
+
+        conn.request("GET", "/oauth/token?response_type=token&grant_type=client_credentials", payload, headers)
+        response = conn.getresponse()
+        data = response.read()
+
+        if response.status == 200:
+            bearer_token = json.loads(data.decode("utf-8")).get('access_token')
+            os.environ['bearer_token'] = bearer_token
+            save_token(bearer_token, datetime.now().isoformat())
+            return bearer_token
+        else:
+            raise Exception(f"Failed to get bearer token: {response.status} {data.decode('utf-8')}")
+
+# X-CSRF-Token API-Aufruf
+def get_xcsrf_token():
+    bearer_token = get_bearer_token()
+    conn = http.client.HTTPSConnection(f"{os.getenv('SACServiceURL')}")
+    headers = {
+        'x-csrf-token': 'fetch',
+        'x-sap-sac-custom-auth': 'true',
+        'Authorization': f'Bearer {bearer_token}'
+    }
+    
+    conn.request("GET", "/api/v1/csrf", headers=headers)
+    response = conn.getresponse()
+    data = response.read()
+    
+    if response.status == 200:
+        xcsrf_token = response.getheader('x-csrf-token')
+        os.environ['xcsrf_token'] = xcsrf_token
+    else:
+        raise Exception(f"Failed to get X-CSRF-Token: {response.status} {data.decode('utf-8')}")

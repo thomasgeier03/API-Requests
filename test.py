468253a8286f9198a0b31a26944b5805e0c6f51c
thomasgeier03 <thomas.geier@siag.it>
2024-08-21 11:17:21 +0200

    Refactor CSV reading and processing in test.py

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ def read_csv_act(file_path):
         for row in reader:
             split = row.get('User Activities').split(',')
             if len(split) > 2:
-                list = split[4]
-                result.add(list)
-        return result
+                print(split[4])
+                result.add(split[4].replace('"', '').replace(',', '').replace('}', ''))
+        return list(result)
 
 def read_csv_user(file_path):
     result = set()
@@ -17,14 +17,19 @@ def read_csv_user(file_path):
         reader = file.readlines()
         for row in reader:
             if row.__contains__('displayName'):
-                list = row.split(':')
-                result.add(list[1].replace('"', '').replace(',', '').replace('}', '').strip())
-    return result
+                split = row.split(':')
+                result.add(split[1].replace('"', '').replace(',', '').replace('}', '').strip())
+        return list(result)
 
 try:
     activities = read_csv_act('output/activities.csv')
     users = read_csv_user('output/users.csv')
+    print("Activities User:")
+    for activity in activities:
+        print(activity)
+    print("\nUsers:")    
     for user in users:
         print(user)
+    
 except FileNotFoundError:
     print("No data available. Run the main function first.")

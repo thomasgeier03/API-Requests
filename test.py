ff96754f4e92750d07ebb758d79ba6ba57c6b495
thomasgeier03 <thomas.geier@siag.it>
2024-08-21 14:45:49 +0200

    Refactor CSV reading and processing in test.py

--- test.py
+++ test.py
@@ -1,35 +1,37 @@
 import csv
 
 def read_csv_act(file_path):
-    result = set()
+    result = []
     with open(file_path, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             split = row.get('User Activities').split(',')
             if len(split) > 2:
-                print(split[4])
-                result.add(split[4].replace('"', '').replace(',', '').replace('}', ''))
-        return list(result)
+                activity = {
+                    split[4].replace('"', '').replace('{', '').strip(),
+                    split[5].replace('"', '').replace('}', '').strip(),
+                    split[7].replace('"', '').replace('}', '').strip()
+                }
+                result.append(activity)
+        return result
 
 def read_csv_user(file_path):
     result = set()
     with open(file_path, 'r') as file:
         reader = file.readlines()
         for row in reader:
-            if row.__contains__('displayName'):
-                split = row.split(':')
-                result.add(split[1].replace('"', '').replace(',', '').replace('}', '').strip())
+            if row.__contains__('Users'):
+                split = row.split('/')
+                result.add(split[5].replace('"', '').replace(',', '').replace('}', '').strip())
         return list(result)
 
 try:
     activities = read_csv_act('output/activities.csv')
     users = read_csv_user('output/users.csv')
-    print("Activities User:")
     for activity in activities:
-        print(activity)
-    print("\nUsers:")    
-    for user in users:
-        print(user)
-    
+        for user in users:
+            if activity.__contains__(user):
+                if len(activity) > 1:
+                    print('Activity:', activity)
 except FileNotFoundError:
     print("No data available. Run the main function first.")
